5a71942196e69bc32d74c5de5c53858c14db24fd
Javier <Javier@Javi>
2017-09-20 13:23:37 +0200

    test

--- mytest/src/main/java/es/magneticdevs/mytest/MyTestUIServlet.java
+++ mytest/src/main/java/es/magneticdevs/mytest/MyTestUIServlet.java
@@ -1,11 +1,8 @@
 package es.magneticdevs.mytest;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 
-@WebServlet(urlPatterns = "/*", name = "MyTestUIServlet", asyncSupported = true)
 @VaadinServletConfiguration(ui = MyTestUI.class, productionMode = false)
 public class MyTestUIServlet extends VaadinServlet {
 }
